86944f3f2b3f23f83c6e9f20fa4f7a7ad5ac5770
Jon Mirtschin <jonm@geometrygym.com>
2019-12-10 17:33:36 +1100

    Initial Validation

---
+++ Core/IFC/Validate.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+using GeometryGym.Mvd;
+
+namespace GeometryGym.Ifc
+{
+	public abstract partial class IfcElement : IfcProduct, IfcStructuralActivityAssignmentSelect
+	{
+		public ValidationResult Validate_IsContainedIn<T>(bool recurseParent) where T : IfcSpatialElement
+		{
+			var containedInStructure = this.ContainedInStructure;
+			if (containedInStructure != null)
+			{
+				var relatingStructure = containedInStructure.RelatingStructure;
+				if (relatingStructure is T)
+					return ValidationResult.Success;
+				if (recurseParent && relatingStructure.Validate_DecomposesOrNests<T>(recurseParent) == ValidationResult.Success)
+						return ValidationResult.Success;
+			}
+			else if (recurseParent)
+			{
+				var decomposes = this.Decomposes;
+				if (decomposes != null)
+				{
+					var relatingObject = decomposes.RelatingObject as IfcElement;
+					if (relatingObject != null)
+						return relatingObject.Validate_IsContainedIn<T>(recurseParent);
+				}
+				var nests = this.Nests;
+				if (nests != null)
+				{
+					var relatingObject = nests.RelatingObject as IfcElement;
+					if (relatingObject != null)
+						return relatingObject.Validate_IsContainedIn<T>(recurseParent);
+				}
+			}
+			return new ValidationResult("Element " + this.StepClassName + " is not contained within spatial element of type " + typeof(T).Name);
+		}
+	}
+	public abstract partial class IfcObjectDefinition : IfcRoot, IfcDefinitionSelect  //ABSTRACT SUPERTYPE OF (ONEOF ((IfcContext, IfcObject, IfcTypeObject))))
+	{
+		public ValidationResult Validate_DecomposesOrNests<T>(bool recurseParent) where T : IfcObjectDefinition
+		{
+			var decomposes = this.Decomposes;
+			if(decomposes != null)
+			{
+				var relatingObject = decomposes.RelatingObject;
+				if (relatingObject is T)
+					return ValidationResult.Success;
+				if (recurseParent)
+					return relatingObject.Validate_DecomposesOrNests<T>(recurseParent);
+			}
+			var nests = this.Nests;
+			if(nests != null)
+			{
+				var relatingObject = nests.RelatingObject;
+				if (relatingObject is T)
+					return ValidationResult.Success;
+				if (recurseParent)
+					return relatingObject.Validate_DecomposesOrNests<T>(recurseParent);
+			}
+			return new ValidationResult("Object " + this.StepClassName + " does not decompose or nest an object of type " + typeof(T).Name);
+		}
+		public Dictionary<IfcRoot, List<ValidationResult>> Validate(List<IConceptRoot> conceptRoots, List<IfcPropertySetTemplate> propertySetTemplates)
+		{
+			Dictionary<IfcRoot, List<ValidationResult>> results = new Dictionary<IfcRoot, List<ValidationResult>>();
+			this.validateWorker(conceptRoots, propertySetTemplates, ref results);
+			return results;
+		}
+		private void validateWorker(List<IConceptRoot> conceptRoots, List<IfcPropertySetTemplate> propertySetTemplates, ref Dictionary<IfcRoot, List<ValidationResult>> results)
+		{
+			List<ValidationResult> validationResults = new List<ValidationResult>();
+			foreach (var propertySetTemplate in propertySetTemplates)
+			{
+				List<ValidationResult> templateResults = validatePropertySetTemplate(propertySetTemplate);
+				if(templateResults != null)
+					validationResults.AddRange(templateResults);
+			}
+
+			foreach (var conceptRoot in conceptRoots)
+			{
+				List<ValidationResult> rootResults = conceptRoot.Validate(this);
+				if(rootResults != null)
+				{
+					validationResults.AddRange(rootResults);
+				}
+			}
+
+			if (validationResults.Count > 0)
+				results[this] = validationResults;
+				
+			foreach (var od in IsDecomposedBy.SelectMany(x => x.RelatedObjects))
+				od.validateWorker(conceptRoots, propertySetTemplates, ref results);
+
+			foreach (var od in IsNestedBy.SelectMany(x => x.RelatedObjects))
+				od.validateWorker(conceptRoots, propertySetTemplates, ref results);
+
+			IfcSpatialElement spatialElement = this as IfcSpatialElement;
+			if (spatialElement != null)
+			{
+				foreach (var relatedElement in spatialElement.ContainsElements.SelectMany(x => x.RelatedElements))
+				{
+					relatedElement.validateWorker(conceptRoots, propertySetTemplates, ref results);
+				}
+				foreach (var relatingSystem in spatialElement.ServicedBySystems.Select(x => x.RelatingSystem))
+					relatingSystem.validateWorker(conceptRoots, propertySetTemplates, ref results);
+			}
+
+		}
+		internal List<ValidationResult> validatePropertySetTemplate(IfcPropertySetTemplate propertySetTemplate)
+		{
+			List<ValidationResult> result = new List<ValidationResult>();
+			if (propertySetTemplate.IsApplicable(this))
+			{
+				IfcPropertySet pset = FindPropertySet(propertySetTemplate.Name) as IfcPropertySet;
+				if (pset == null)
+					result.Add(new ValidationResult("Missing Property Set " + propertySetTemplate.Name));
+				else
+				{
+					foreach (var propertyTemplate in propertySetTemplate.HasPropertyTemplates.Values)
+					{
+						IfcSimplePropertyTemplate simplePropertyTemplate = propertyTemplate as IfcSimplePropertyTemplate;
+						if(simplePropertyTemplate != null)
+						{
+							if (simplePropertyTemplate.TemplateType == IfcSimplePropertyTemplateTypeEnum.P_SINGLEVALUE)
+							{
+								IfcPropertySingleValue property = pset.FindProperty(propertyTemplate.Name) as IfcPropertySingleValue;
+								if (property == null)
+									result.Add(new ValidationResult("Missing Property " + propertySetTemplate.Name + " : " + propertyTemplate.Name));
+								else
+								{
+									IfcValue value = property.NominalValue;
+									if (value == null)
+										result.Add(new ValidationResult("Missing Property Value " + propertySetTemplate.Name + " : " + propertyTemplate.Name));
+									else
+									{
+										string valueString = value.ValueString;
+										if (string.IsNullOrEmpty(valueString))
+											result.Add(new ValidationResult("Missing Property Value " + propertySetTemplate.Name + " : " + propertyTemplate.Name));
+										else
+										{
+											string primary = simplePropertyTemplate.PrimaryMeasureType;
+											if (!string.IsNullOrEmpty(primary))
+											{
+												string valueClass = value.StepClassName;
+												if (string.Compare(valueClass, primary, true) != 0)
+													result.Add(new ValidationResult(" Unexpected value type " + valueClass + " for " + propertySetTemplate.Name + " : " + propertyTemplate.Name));
+											}
+										}
+									}
+								}
+							}
+							else
+								result.Add(new ValidationResult("!!! Unhandled PropertyTemplate " + propertyTemplate.ToString()));
+						}
+						else
+							result.Add(new ValidationResult("!!! Unhandled PropertyTemplate " + propertyTemplate.ToString()));
+					}
+				}
+			}
+			return result;
+		}
+
+		public ValidationResult ValidateIsObject<T>(bool acceptSubTypes) where T : IfcObjectDefinition 
+		{
+			if (this is T)
+			{
+				if (!acceptSubTypes && this.GetType().IsSubclassOf(typeof(T)))
+					return new ValidationResult("Object (" + StepClassName + ") is not " + typeof(T).Name);
+				return ValidationResult.Success;
+			}
+			return new ValidationResult("Object (" + StepClassName + ") is not " + typeof(T).Name);
+		}
+	}
+	public abstract partial class IfcProduct : IfcObject, IfcProductSelect
+	{
+		private IfcRepresentationItem SingleBodyRepresentationItem(ref ValidationResult validation)
+		{
+			var representation = Representation;
+			if (representation == null)
+			{
+				validation = new ValidationResult("Missing Product Representation");
+				return null;
+			}
+			var representations = representation.Representations.Where(x => string.Compare(x.RepresentationIdentifier, "Body", true) == 0).ToList();
+			if (representations.Count == 0)
+			{
+				validation = new ValidationResult("Missing Body Representation");
+				return null;
+			}
+			if (representations.Count > 1)
+			{
+				validation = new ValidationResult("Multiple Body Representations");
+				return null;
+			}
+			List<IfcRepresentationItem> items = representations[0].Items.ToList();
+			if (items.Count == 0)
+			{
+				validation = new ValidationResult("Missing Body Representation");
+				return null;
+			}
+			if (items.Count > 1)
+			{
+				validation = new ValidationResult("Multiple Body Representations");
+				return null;
+			}
+			return items[0];
+		}
+		public ValidationResult Validate_SingleBodyRepresentation<T>(bool acceptSubTypes) where T : IfcRepresentationItem
+		{
+			ValidationResult result = null;
+			IfcRepresentationItem singleModelRepresenationItem = SingleBodyRepresentationItem(ref result);
+			if (singleModelRepresenationItem == null)
+				return result;
+			if (!(singleModelRepresenationItem is T))
+				return new ValidationResult("Single model representation item (" + singleModelRepresenationItem.StepClassName + ") is not " + typeof(T).Name);
+			if(!acceptSubTypes)
+			{
+				if(singleModelRepresenationItem.GetType().IsSubclassOf(typeof(T)))
+					return new ValidationResult("Single model representation item (" + singleModelRepresenationItem.StepClassName + ") is subclass of " + typeof(T).Name);
+			}
+
+			return ValidationResult.Success;
+		}
+		public ValidationResult Validate_SingleBodyRepresentation<T, U>(bool acceptSubTypes) where T : IfcRepresentationItem where U : IfcRepresentationItem
+		{
+			ValidationResult result = null;
+			IfcRepresentationItem singleModelRepresenationItem = SingleBodyRepresentationItem(ref result);
+			if (singleModelRepresenationItem == null)
+				return result;
+			if (!(singleModelRepresenationItem is T || singleModelRepresenationItem is U))
+				return new ValidationResult("Single model representation item (" + singleModelRepresenationItem.StepClassName + ") is not " + typeof(T).Name + " or " + typeof(U).Name);
+			if (!acceptSubTypes)
+			{
+				if(singleModelRepresenationItem.GetType().IsSubclassOf(typeof(T)))
+					return new ValidationResult("Single model representation item (" + singleModelRepresenationItem.StepClassName + ") is subclass of " + typeof(T).Name);
+				if(singleModelRepresenationItem.GetType().IsSubclassOf(typeof(U)))
+					return new ValidationResult("Single model representation item (" + singleModelRepresenationItem.StepClassName + ") is subclass of " + typeof(U).Name);
+			}
+			return ValidationResult.Success;
+		}
+		public ValidationResult Validate_SingleBodyRepresentation<T, U, V>(bool acceptSubTypes) where T : IfcRepresentationItem where U : IfcRepresentationItem where V : IfcRepresentationItem
+		{
+			ValidationResult result = null;
+			IfcRepresentationItem singleModelRepresenationItem = SingleBodyRepresentationItem(ref result);
+			if (singleModelRepresenationItem == null)
+				return result;
+			if (!(singleModelRepresenationItem is T || singleModelRepresenationItem is U || singleModelRepresenationItem is V))
+				return new ValidationResult("Single Body representation item (" + singleModelRepresenationItem.StepClassName + ") is not " + typeof(T).Name + " or " + typeof(U).Name + " or " + typeof(V).Name);
+			if (!acceptSubTypes)
+			{
+				if(singleModelRepresenationItem.GetType().IsSubclassOf(typeof(T)))
+					return new ValidationResult("Single Body representation item (" + singleModelRepresenationItem.StepClassName + ") is subclass of " + typeof(T).Name);
+				if(singleModelRepresenationItem.GetType().IsSubclassOf(typeof(U)))
+					return new ValidationResult("Single Body representation item (" + singleModelRepresenationItem.StepClassName + ") is subclass of " + typeof(U).Name);
+				if(singleModelRepresenationItem.GetType().IsSubclassOf(typeof(V)))
+					return new ValidationResult("Single Body representation item (" + singleModelRepresenationItem.StepClassName + ") is subclass of " + typeof(V).Name);
+			}
+			return ValidationResult.Success;
+		}
+	}
+}
---
+++ Core/MVD/MVD.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+using GeometryGym.Ifc;
+
+namespace GeometryGym.Mvd
+{
+	public enum StatusEnum
+	{
+		Sample = 0, // default
+		Proposal = 1,
+		Draft = 2,
+		Candidate = 3,
+		Final = 4,
+		Deprecated = -1,
+	}
+	public abstract class Identity
+	{
+		public Guid Uuid { get; set; }
+		public string Name { get; set; }
+		public string Code { get; set; } // e.g. 'bsi-100'
+		public string Version { get; set; }
+		public StatusEnum Status { get; set; } // e.g. 'draft'
+		public string Author { get; set; }
+		public string Owner { get; set; } // e.g. 'buildingSMART international'
+		public string Copyright { get; set; }
+
+		public bool ShouldSerializeUuid()
+		{
+			return Uuid != Guid.Empty;
+		}
+		public bool ShouldSerializeStatus()
+		{
+			return Status != StatusEnum.Sample;
+		}
+		public Identity(string name, Guid uuid)
+		{
+			Name = name;
+			Uuid = uuid;
+		}
+	}
+	public abstract class Element : Identity
+	{
+		public List<Definition> Definitions { get; set; }
+
+		public Element(string name, Guid uuid)
+			: base(name, uuid)
+		{
+			Definitions = new List<Definition>();
+		}
+
+		public bool ShouldSerializeDefinitions()
+		{
+			return null != this.Definitions && this.Definitions.Any();
+		}
+	}
+	public enum ApplicabilityEnum
+	{
+		Both = 0,
+		Export = 1,
+		Import = 2,
+	}
+
+	public enum RequirementEnum
+	{
+		Mandatory = 1,
+		Recommended = 2,
+		NotRelevant = 3,
+		NotRecommended = 4,
+		Excluded = 5,
+	}
+	public delegate bool TemplateRule<T>(T item);
+
+	public class BaseConcept
+	{
+		public Guid Ref { get; set; }
+	}
+	public class Concept<T> : Element where T : IfcRoot
+	{
+		public TemplateRef Template { get; set; }// links to ConceptTemplate 
+		public RequirementEnum Requirement { get; set; }
+		public TemplateRule<T> TemplateRules { get; set; }
+
+		public List<Concept<T>> SubConcepts { get; set; }// added v3.8
+		public bool Override { get; set; }// added in v5.6
+		public BaseConcept BaseConcept { get; set; }
+
+		public Concept(string name, Guid uuid)
+			: base(name, uuid)
+		{
+			SubConcepts = new List<Concept<T>>();
+		}
+		public bool ShouldSerializeSubConcepts()
+		{
+			return null != this.SubConcepts && this.SubConcepts.Any();
+		}
+	}
+
+	public interface IConceptRoot
+	{
+		List<ValidationResult> Validate(IfcRoot root);
+	}
+	public abstract class ConceptRoot<T> : Element, IConceptRoot where T : IfcRoot
+	{
+		public ApplicabilityRules<T> Applicability { get; set; }
+		public List<Concept<T>> Concepts { get; set; }
+
+		public ConceptRoot(Guid guid)
+			: base("", guid)
+		{
+			Name = this.GetType().Name;
+		}
+
+		public List<ValidationResult> Validate(IfcRoot root)
+		{
+			T obj = root as T;
+			if (obj == null)
+				return null;
+			// Test Applicability
+			List<ValidationResult> results = new List<ValidationResult>();
+			foreach(ValidationResult result in ValidateWorker(obj))
+			{
+				if (result == ValidationResult.Success)
+					continue;
+				results.Add(new ValidationResult(Uuid.ToString() + " | " + Name + " | " + result.ErrorMessage));
+			}
+			return results;
+		}
+		protected abstract List<ValidationResult> ValidateWorker(T obj);
+	}
+
+	public class ApplicabilityRules<T> where T : IfcRoot
+	{
+		public Definition Definitions { get; set; }
+		public TemplateRef Template { get; set; }
+		public TemplateRule<T> TemplateRule { get; set; }
+	}
+	public class Definition
+	{
+		public string Body { get; set; }
+		public List<Link> Links { get; set; }
+		public string Tags { get; set; }
+
+		public Definition()
+		{
+			Links = new List<Link>();
+		}
+	}
+
+	public enum CategoryEnum
+	{
+		definition = 0,
+		agreement = 1,
+		diagram = 2,
+		instantiation = 3,
+		example = 4,
+	}
+	public class Link
+	{
+		public string Lang { get; set; }
+		public CategoryEnum Category { get; set; }
+		public string Title { get; set; }
+		public string Href { get; set; }
+		public string Content { get; set; }
+	}
+
+	public class TemplateRef
+	{
+		public Guid Ref { get; set; }
+	}
+
+
+
+	public class ExchangeRequirements
+	{
+		protected List<IConceptRoot> Concepts { get; }
+		protected List<IfcPropertySetTemplate> PropertyTemplates { get; }
+
+		public List<IfcClassificationReference> ApplicableProcess { get; }
+		public List<IfcClassificationReference> Senders { get; }
+		public List<IfcClassificationReference> Receivers { get; }
+		public List<IfcClassificationReference> Activities { get; }
+
+		public ExchangeRequirements()
+		{
+			Concepts = new List<IConceptRoot>();
+			PropertyTemplates = new List<IfcPropertySetTemplate>();
+			ApplicableProcess = new List<IfcClassificationReference>();
+			Senders = new List<IfcClassificationReference>();
+			Receivers = new List<IfcClassificationReference>();
+			Activities = new List<IfcClassificationReference>();
+		}
+
+		public Dictionary<IfcRoot, List<ValidationResult>> Validate(IfcObjectDefinition objectDefinition)
+		{
+			return objectDefinition.Validate(Concepts, PropertyTemplates);
+		}
+
+	}
+
+	public class ConceptGridAttributes : ConceptRoot<IfcGrid>
+	{
+		public ConceptGridAttributes()
+			: base(new Guid("9c7b470d-978a-40fa-a777-449b40588800"))
+		{
+
+		}
+		protected override List<ValidationResult> ValidateWorker(IfcGrid obj)
+		{
+			List<ValidationResult> result = new List<ValidationResult>();
+			foreach (IfcGridAxis axis in obj.UAxes)
+			{
+				if (string.IsNullOrEmpty(axis.AxisTag))
+					result.Add(new ValidationResult("Empty U Axis Tag"));
+				ValidationResult validation = ValidateGridAxisCurve(axis);
+				if (validation != ValidationResult.Success)
+					result.Add(validation);
+			}
+			foreach (IfcGridAxis axis in obj.VAxes)
+			{
+				if (string.IsNullOrEmpty(axis.AxisTag))
+					result.Add(new ValidationResult("Empty V Axis Tag"));
+				ValidationResult validation = ValidateGridAxisCurve(axis);
+				if (validation != ValidationResult.Success)
+					result.Add(validation);
+			}
+			foreach (IfcGridAxis axis in obj.WAxes)
+			{
+				if (string.IsNullOrEmpty(axis.AxisTag))
+					result.Add(new ValidationResult("Empty W Axis Tag"));
+				ValidationResult validation = ValidateGridAxisCurve(axis);
+				if (validation != ValidationResult.Success)
+					result.Add(validation);
+			}
+			return result;
+		}
+
+		private ValidationResult ValidateGridAxisCurve(IfcGridAxis axis)
+		{
+			if (axis.AxisCurve is IfcIndexedPolyCurve || axis.AxisCurve is IfcCircle)
+				return ValidationResult.Success;
+			return new ValidationResult("");
+		}
+	}
+
+}
